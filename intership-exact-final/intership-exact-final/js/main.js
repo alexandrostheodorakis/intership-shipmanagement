81cc423cacb2977a6d463508c172f5859dd28792
alexandrostheodorakis <alexandrostheodorakis89@gmail.com>
2026-09-22 13:36:00 +0300

    Delete intership-exact-final/intership-exact-final directory

--- intership-exact-final/intership-exact-final/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("a[href^='#']").forEach(a => {
-    a.addEventListener("click", e => {
-      const target = document.querySelector(a.getAttribute("href"));
-      if (target) { e.preventDefault(); target.scrollIntoView({behavior:"smooth"}); }
-    });
-  });
-});
